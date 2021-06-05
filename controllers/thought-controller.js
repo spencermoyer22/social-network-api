b84030648d6b61e3365a153c5d9af192f420d163
Big Head <spencer.moyer16@yahoo.com>
2021-06-05 00:56:32 -0700

    created api routes for reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -63,6 +63,34 @@ const thoughtController = {
             res.json(dbUserData);
         })
         .catch(err => res.json(err));
+    },
+    createReaction({params, body}, res) {
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId},
+            {$push: {reactions: body}},
+            {new: true, runValidators: true}
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                return res.status(404).json({message: "No thought found with this id"})
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(500).json(err));
+    },
+    deleteReaction({params}, res) {
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId},
+            {$pull: {reactions: {reactionId: params.reactionId}}},
+            {new: true}
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                return res.status(404).json({message: 'No thought found with this id'})
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(500).json(err));
     }
 }
 
