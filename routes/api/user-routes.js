43bf96a16afb31e62878ec151371b41269214959
Big Head <spencer.moyer16@yahoo.com>
2021-06-07 00:15:51 -0700

    fixed friend routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const {createUser, getAllUsers, getOneUser, updateUser, deleteUser} = require('../../controllers/user-controller');
+const {createUser, getAllUsers, getOneUser, updateUser, deleteUser, addFriend, removeFriend} = require('../../controllers/user-controller');
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getOneUser).put(updateUser).delete(deleteUser);
+router.route('/:userId/friends/:friendId').put(addFriend).delete(removeFriend);
 
 module.exports = router;
